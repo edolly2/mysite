1ab0a153a39d53d6b6a26092ba028ddeb88eb06f
edolly2 <edolly2@protonmail.com>
2021-04-27 04:25:48 -0500

    ""

--- js/script.js
+++ js/script.js
@@ -4,14 +4,20 @@ var bigNavLink = $('.top-navbar-link');
 var smallNavLink = $('.whisker-menu-link');
 var showShieldTop = $('.tls');
 var showShieldW = $('.wms');
-
+var card1 = $('.card-1-container');
+var card2 = $('.card-2-container');
+var card3 = $('.card-3-container');
+var p1 = $('.p1');
+var p2 = $('.p2');
+var p3 = $('.p3');
+var learnMore = $('.learn-more-icon-container');
 
 
 $(window).resize(() => {
     if ($(window).width() > 470) {
         $(whiskerMenu).hide();
         $(whiskerBtn).removeClass('rotate-90');
-    }
+    };
 
 }).resize();
 
@@ -24,12 +30,12 @@ $(whiskerBtn).on('click', () => {
 $(bigNavLink).on('click', (event) => {
     $(bigNavLink).removeClass('is-active');
     $(event.currentTarget).addClass('is-active');
-})
+});
 
 $(smallNavLink).on('click', (event) => {
     $(smallNavLink).removeClass('is-active');
     $(event.currentTarget).addClass('is-active');
-})
+});
 
 $(bigNavLink).click((event) => {
     $(showShieldTop).hide();
@@ -56,7 +62,7 @@ $(bigNavLink).click((event) => {
         $('.wm5').addClass('is-active');
         $('.ws5').show();
     }
-})
+});
 
 $(smallNavLink).click((event) => {
     $(showShieldW).hide();
@@ -83,4 +89,52 @@ $(smallNavLink).click((event) => {
         $('.tl5').addClass('is-active');
         $('.ts5').show();
     }
-})
+});
+
+$(window).on('load', () => {
+    $(card1).delay(2000).fadeIn(3000);
+    $(card2).delay(5000).fadeIn(3000);
+    $(card3).delay(8000).fadeIn(3000);
+});
+
+// if($('.card-1-p').is(!'visible:')){
+//     $('.card-1-p').addClass('mh208');
+// }
+// }
+// else {
+//     $('.cc').removeClass('mh208');
+// }
+
+$(learnMore).on('click', (event) => {
+    $('.cc').addClass('mh208');
+    if($(event.currentTarget).hasClass('p1')) {
+        $('.card-1-container').toggleClass('mh208');
+        $('.card-1-p').toggle();
+        $('.fsd1').toggleClass('rotate-180');
+    }
+    else if($(event.currentTarget).hasClass('p2')) {
+        $('.card-2-container').toggleClass('mh208');
+        $('.card-2-p').toggle();
+        $('.fsd2').toggleClass('rotate-180');
+    }
+    else if($(event.currentTarget).hasClass('p3')) {
+        $('.card-3-container').toggleClass('mh208');
+        $('.card-3-p').toggle();
+        $('.fsd3').toggleClass('rotate-180');
+    }
+});
+
+if($(window).width() > 600) {
+    $('.cp').show();
+}
+// $('.fa-sort-down').show();
+// if($('.card-1-container').hasClass('mh208')) {
+    // $('.fa-sort-down').hide();
+    // $('.fa-sort-up').hide();
+
+
+// } 
+// else {
+//     $('.fa-sort-up').show();
+    // $('.fa-sort-down').hide();
+// }
